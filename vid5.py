7e12035491970fa638094c47f65889572402c49f
Wahdaapf <wahdaadella.ba@gmail.com>
2024-01-12 06:11:26 +0700

    flash message

--- vid5.py
+++ vid5.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "hello"
 app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
@@ -14,9 +15,11 @@ def test():
         session.permanent = True
         user = request.form['nm']
         session["user"] = user
+        flash("Login Successfull")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash("Already Login")
             return redirect(url_for("user"))
         return render_template("login.html")
 
@@ -24,12 +27,14 @@ def test():
 def user():
     if "user" in session:
         user = session['user']
-        return f"Hello {user}"
+        return render_template("user.html", user=user)
     else:
+        flash("Login First")
         return render_template("login.html")
     
 @app.route("/logout")
 def logout():
+    flash(f"you have been log out!", "info")
     session.pop("user", None)
     return render_template("login.html")
 
